c56b38e15bcc425a8ee6826621fbb2beea8b2570
ArtyomKorotkov <artemcka.korotkov@yandex.ru>
2023-09-25 23:07:18 +0300

    input and output ks

--- KorotkovAs2204 lab1/KorotkovAs2204 lab1.cpp
+++ KorotkovAs2204 lab1/KorotkovAs2204 lab1.cpp
@@ -14,12 +14,11 @@ t getCorrectNumber(t min, t max)
     }
     return x;
 }
-
 struct Pipe{
     string name;
     double length;
     double diameter;
-    bool repair;
+    string repair;
 };
 
 struct KS {
@@ -36,8 +35,21 @@ istream& operator >> (istream& in, Pipe& pipe)
     pipe.length = getCorrectNumber(50, 200);
     cout << "Type diameter:";
     pipe.diameter = getCorrectNumber(8, 35);
-    cout << "Is this pipe in repair?(Yes-0,No-1)";
-    pipe.repair = getCorrectNumber(0, 1);
+    cout << "Is this pipe in repair?(Yes-1,No-0)";
+    pipe.repair = (getCorrectNumber(0, 1)==1) ? "Yes":"No";
+    return in;
+}
+istream& operator >> (istream& in, KS& ks)
+{
+    cout << "Type name:";
+    in >> ks.name;
+    cout << "Type how much shop you have:";
+    ks.amoutOfShop = getCorrectNumber(0, 10000) ;
+    cout << "Type how much working shop you have:";
+    ks.amountWorkingShop = getCorrectNumber(0, ks.amoutOfShop);
+    double usefulA = ks.amountWorkingShop;
+    double A = ks.amoutOfShop;
+    ks.KPD = usefulA / A;
     return in;
 }
 ostream& operator << (ostream& out, const Pipe& pip)
@@ -48,6 +60,15 @@ ostream& operator << (ostream& out, const Pipe& pip)
         << "\tRepair:" << pip.repair << endl;
     return out;
 }
+ostream& operator << (ostream& out, const KS& ks)
+{
+    out << "Name:" << ks.name
+        << "\tAmount of shop:" << ks.amoutOfShop
+        << "\tAmount of working shop:" << ks.amountWorkingShop
+        << "\tKPD:" << ks.KPD << endl;
+    return out;
+}
+
 void PrintMenu()
 {
     cout << "1. Add pipe" << endl
@@ -77,6 +98,9 @@ int main()
         }
         case 2:
         {
+            KS ks;
+            cin >> ks;
+            muchKS.push_back(ks);
             break;
         }
         case 3:
@@ -84,6 +108,9 @@ int main()
             cout << "Pipes:" << endl;
             for (auto& pip : muchPipes)
                 cout << pip << endl;
+            cout << "KS:" << endl;
+            for (auto& ks : muchKS)
+                cout << ks << endl;
             break;
         }
         case 4:
@@ -105,10 +132,6 @@ int main()
         case 0:
         {
             return 0;
-        }
-        default:
-        {
-            cout << "Wrong action" << endl;
         }
         }
 
